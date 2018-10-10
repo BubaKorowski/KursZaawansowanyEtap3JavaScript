ae8742cd84950509573f7edb23696f0960bc732a
Boba Korowski <Jakub.B.Borowski@gmail.com>
2018-10-10 14:31:10 +0200

    funkcje_wartościDomyślne_callback_objectArbuments

--- 1.7_funkcje/script.js
+++ 1.7_funkcje/script.js
@@ -12,10 +12,10 @@
 
 // const nazwaFunkcjiCoRobi = function() {};
 
-const showMessage = function(message) {
-  console.log(message);
-};
-showMessage("Pokazuję wiadomość");
+// const showMessage = function (message) {
+//   console.log(message);
+// };
+// showMessage("Pokazuję wiadomość");
 
 // 2. DEKLARACJA FUNKCJI - ndeviajpierw słowo kluczowe function a potem nazwa. Function Declaration.
 
@@ -69,51 +69,145 @@ showMessage("Pokazuję wiadomość");
 
 // example 1 - Funkcja która wpływa na program(efekt uboczny) i zwraca undefined
 
-const divideBy2 = function(number) {
-  console.log(number / 2);
-};
+// const divideBy2 = function (number) {
+//   console.log(number / 2);
+// };
 
-const result1 = divideBy2(10);
+// const result1 = divideBy2(10);
 //do zmiennej result1 zostało przypisane undefined
 
 // example 2 - Funkcja która tylko przetwarza dane(bez efektu ubocznego) i zwraca wartość
-const devideBy3 = function(number) {
-  return number / 3;
-};
-const result2 = devideBy3(9);
-console.log("Wynik to " + result2);
+// const devideBy3 = function (number) {
+//   return number / 3;
+// };
+// const result2 = devideBy3(9);
+// console.log("Wynik to " + result2);
 //w powyższym przypadku resut2 przechowuje zwruconą wartość a nie undefined
 
 // example 3 Funkcja z efektem ubocznym i zwracająca wartość
 
-const divideBy5 = function(number) {
-  const result = number / 5;
-  console.log(result);
-  return result;
-};
+// const divideBy5 = function (number) {
+//   const result = number / 5;
+//   console.log(result);
+//   return result;
+// };
 
 // PARAMETRY I ARGUMENTY
 
 // parametr to element składowy funkcji na etapie jej tworzenia. Nie trzeba ich umieszczać (funkcja nie musi mieć parametrów)
 
 //bez parametru
-const showMyName = function() {
-  console.log("Jakub");
-};
+// const showMyName = function () {
+//   console.log("Jakub");
+// };
 
 //z parametrem
 // Parametr - pojęcie używane dla określenia nazwy używanej przy twworzeniu funkcji
-const showUserName = function(userName) {
-  console.log(userName);
-};
+// const showUserName = function (userName) {
+//   console.log(userName);
+// };
 
 //Argument - pojęcie użyte przy wywołaniu funkcji
-showUserName("Izabella");
+// showUserName("Izabella");
 
 //W funnkcji powyżej tworząc funkcję ustawiamy jede n parametr. Przy wywołaniu  miejsce parametru podajemy argument, który pełni rolę wartości dla parametru.
 
 //Jeśli nie podamy argumentu to w jego miejsce zostanie wstawione undefined
 
 //Jeśli podamy więcej argumentów niż parametrów to nie spowoduje to błędu
-showUserName("piotr", 200);
-showUserName.length; // pokazuje ilu argumentów oczekuje dana funkcja(czyli ile paramatrów posiada w momencie jej tworzenia)
+// showUserName("piotr", 200);
+// showUserName.length; // pokazuje ilu argumentów oczekuje dana funkcja(czyli ile paramatrów posiada w momencie jej tworzenia)
+
+// WARTOŚCI DOMYŚLNE W ES6
+
+// jeśli nie podamy argumentów na etapie wywołania funkcji to funkcja weźmie argumentu domyślne
+
+// const addTwoNumbers = (x = 2, y = 3) => {
+//   const result = x + y;
+//   console.log(result);
+//   return result;
+// }
+
+// Deklaracja funkcji (poniżej to samo co powyżej, ale inny sposób tworzenia)
+
+// const addTwoNumbers2 = function (x = 0, y = 3) {
+//   const result = x + y;
+//   console.log(result);
+//   return result;
+// }
+
+// CALLBACK
+// Jest to funkcja wywołania zwrotnego, czyli funkcja przekazana jako argument do funkcji i wywołana w niej
+// Funkcja do której przekazujemy inną funkcję, czyli nasz callback, nazywa się funkcją wyższego rzędu.
+
+//1. Przykład CALLBACK
+
+// function count(x, callback) {
+//   return callback(x)
+// }
+
+// function addOne(number) {
+//   number++;
+//   console.log(number);
+//   return number;
+// }
+
+
+// function subtractOne(number) {
+//   number--;
+//   console.log(number);
+//   return number;
+// }
+// const result = count(4, addOne)
+
+// 2. Przykład CALLBACK
+
+// document.body.addEventListener("click", function () {
+//   console.log("klikinąłeś")
+// })
+
+// 3. Przykład CALLBACK
+
+// setInterval(() => console.log("minęła 1 sekunda"), 1000)]
+
+//albo
+
+// const showTime = () => {
+//   console.log("minęła 1 sekunda")
+// }
+// setInterval(showTime, 1000)
+
+// 3. Przykład CALLBACK
+
+const usersAge = [20, 30, 40, 50, 60, 70, 80];
+
+// usersAge.forEach(userAge => console.log(`Wiek użytkownika to ${userAge}`))
+
+//albo
+
+// usersAge.forEach(function (userAge) {
+//   console.log("wiek użytkownika to " + userAge)
+// })
+
+// ARGUMENTS - obiekt ARGUMENTS zawiera aktualną listę argumentów przekazanych do tablicy. (podobny do tablicy)
+
+const showArguments = function () {
+  console.log(arguments) //arguments - nazwa dla obiektu (nazwa przypisana)
+  console.log(arguments.length) //długość - ile argumentów przekazaliśmy
+  console.log(arguments[0]) //odwołanie do konkretnego argumentu
+  console.log(typeof arguments) //obiekt (tablico-podobny). Ma właściwość length, można odwołać się za pomocą notacji tablicowej np. arguments[2]. Nie ma metod znanych z tablicy
+}
+
+// showArguments("5", null, {})
+
+//wykorzystanie w praktyce
+
+const addAllNumbers = function () {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
+}
+
+const usersMoney = addAllNumbers(2, 2, 2);
