52de3e48c298d23434043458a6618ad14b3ff2ae
Buba Korowski <Jakub.B.Borowski@gmail.com>
2018-11-01 08:08:04 +0100

    toDoList 1st lesson

---
+++ 3.242_to do list_wyszukiwanie elementów/main.js
@@ -0,0 +1,31 @@
+//  FILTER (PRZYPOMNIENIE MAP)
+
+const arr = [23, 4255, 34656, 4, 33, 57767, 8877, 42];
+
+//  Filter
+const oddNumbers = arr.filter(number => number % 2);
+const evenNumbers = arr.filter(number => !(number % 2));
+const numbersBiggerThan100 = arr.filter(number => number > 100);
+
+//  Map
+const duble = arr.map(number => number * 2);
+
+//  forEach
+arr.forEach((number, index) => (arr[index] = number * 2));
+
+//  SEARCH
+
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const liElements = document.querySelectorAll("li");
+
+const searchTask = e => {
+  const searchText = e.target.value.toLowerCase();
+  let tasks = [...liElements];
+  tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText));
+  console.log(tasks);
+  ul.textContent = "";
+  tasks.forEach(li => ul.appendChild(li));
+};
+
+input.addEventListener("input", searchTask);
