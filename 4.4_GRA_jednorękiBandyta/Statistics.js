2bbf51f9d336d4c6e6fdf62f46230af4d585d28e
Buba Korowski <Jakub.B.Borowski@gmail.com>
2018-11-17 18:40:29 +0100

    game

---
+++ 4.4_GRA_jednorękiBandyta/Statistics.js
@@ -0,0 +1,22 @@
+class Statistics {
+  constructor() {
+    this.gameResults = [];
+  }
+
+  addGameToStatistics(win, bid) {
+    let gameResult = {
+      win,
+      bid
+    };
+    console.log(gameResult);
+    this.gameResults.push(gameResult);
+  }
+
+  showGaameStatistics() {
+    let games = this.gameResults.length;
+    let wins = this.gameResults.filter(result => result.win).length;
+    let losses = this.gameResults.filter(result => !result.win).length;
+    return [games, wins, losses];
+  }
+}
+const stats = new Statistics();
